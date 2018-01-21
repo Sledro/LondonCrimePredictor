8ee10f35a2b007dd8d1e496a90a0ce0515ec9f10
sledro <dan@sledro.com>
2018-01-21 22:18:45 +0000

    Fetching data from firebase.

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from app.fireTest import fire
 
 @app.route('/')
 @app.route('/index')
@@ -15,4 +16,6 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    print(fire())
+    return render_template('index.html', title='Home', user=user, posts=posts, fire=fire())
+
