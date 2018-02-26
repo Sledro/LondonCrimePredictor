c976c7bf283b6c7093252dc9ebcbb1374b8025a1
sledro <dan@sledro.com>
2018-02-26 12:45:45 +0000

    Jupyter notebooks uploaded, webapp uploaded

--- app/plot.py
+++ app/plot.py
@@ -7,15 +7,31 @@ plotly.tools.set_credentials_file(username='Sledro', api_key='b78t47VjKxyv1HTh9G
 import plotly.plotly as py
 import cufflinks as cf
 import pandas as pd
+from sklearn.linear_model import LinearRegression
+import numpy as np
+#Set width of terminal output
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
 
-cf.set_config_file(offline=False, world_readable=True, theme='ggplot')
+#Define dateparse as our data sets date fornat Y-M
+dateparse = lambda dates: pd.datetime.strptime(dates, '%Y-%m')
+#Read data from csv and hold it in our data frame
+df = pd.read_csv('/Users/sledro/Desktop/LondonCrime/Datasets with access/Cleaned.csv', parse_dates=['Month'], date_parser=dateparse)
+#Group our data by the number of each type of crimes commit each month
+groupedCrimes= df.set_index('Month', drop=False).groupby(pd.Grouper(level='Month', freq='M'))['Crime type'].value_counts().unstack()
 
-df = pd.read_csv('/Users/sledro/Desktop/LondonCrime/Datasets with access/Cleaned.csv', parse_dates=True, index_col=1)
-df.head(3)
-
-series = df['Crime type'].value_counts()[:20]
-print(series.head(3))
+#I change the dates to be integers, I am not sure this is the best way    
+df['Month'] = pd.to_datetime(df['Month'])  
+df['date_delta'] = (df['Month'] - df['Month'].min())  / np.timedelta64(1,'D')
 
 
-
-#series.iplot(kind='bar', yTitle='Number of Complaints', title='NYC 311 Complaints',filename='cufflinks/categorical-bar-chart')
+model = LinearRegression()
+y = groupedCrimes.Burglary
+print(y.shape)
+X = df[['date_delta']]
+print(X.shape)
+model.fit(X, y)
+model.score(X, y)
+coefs = zip(model.coef_, X.columns)
+print ("sl = %.1f + " % model.intercept_ + \
+" + ".join("%.1f %s" % coef for coef in coefs))
